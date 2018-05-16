b9556f1471eecbd855b092c121464660339b15d1
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-15 22:22:12 -0700

    working on front end

--- AngularApp/src/app/app.component.ts
+++ AngularApp/src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Rate My Cakes;
+  cakes = [];
+  ratings = [];
+  showCake = {};
+  newCake: any;
+
+  constructor(private _httpService: HttpService) {}
+  cakesFromService(): void {
+    const observable = this._httpService.getCakes();
+    observable.subscribe(data => {
+      console.log('Look at my cakes', data);
+      this.cakes = data['data'];
+    });
+  }
+
+  showCake(id: string) {
+    const observable = this._httpService.showCake(id);
+    observable.subscribe(data => this.showCake = data['data']);
+  }
+
+  addCake(id: string) {
+    const observable = this._httpService.addCakes(this.newCake);
+    observable.subscribe(data => {
+      console.log('new cake', data);
+      this.newCake = {baker: '', img: ''};
+      this.cakesFromService();
+    });
+  }
+
+  addRating(id: string) {
+    const observable = this._httpService.addRating(this.showCake);
+    observable.subscribe(data => {
+      console.log('adding rating', data);
+      // this.showCake = {rating: '', img: ''};
+      this.ratings = data['data'];
+    });
+  }
+
+  ngOnInit() {
+    this.newCake = { baker: '', img: '' };
+  }
+
 }
